2eab06d7378c62ada602606185814f834fdf3dc3
markalabastro <42789917+markalabastro@users.noreply.github.com>
2022-03-11 10:24:15 -0600

    Fix add function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
+/* 
+1.push value into an Array
+2.push operator into an array
+3.push new value into an array
+4.unshift value;
+5.unshift operator;
+6.unshift value;
+7.send to operate to calculate amount
+8.
+*/
+
 function add(a, b) {
-  a = parseInt(a);
-  b = parseInt(b);
   return a + b;
 }
 
@@ -17,8 +26,6 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-  console.log(operator, a, b);
-
   if (operator == "+") {
     return add(a, b);
   } else if (operator === "-") {
@@ -30,38 +37,61 @@ function operate(operator, a, b) {
   }
 }
 
+function getNumber(e) {
+  let addDigit = e.target.value;
+  numberString += addDigit;
+  let displayWindow = numberString;
+  display.textContent = parseInt(displayWindow);
+}
+
 function clear() {
+  equationArray = [];
   numberString = "";
-  display.value = "";
+  numberString2 = "";
+  display.textContent = "";
 }
 
 function changeSign() {
   console.log(numberString);
   numberString = numberString * -1;
-  display.value = numberString;
+  display.textContent = numberString;
 }
 
 let numberString = "";
+let numberString2 = "";
+let equationArray = [];
 let display = document.querySelector("#display");
 let clearButton = document.getElementById("clear");
 let addButton = document.getElementById("add");
 let changeSignButton = document.getElementById("changeSign");
+let equalsButton = document.getElementById("equals");
 let numberButtons = document.querySelectorAll(`button[data-num^='num']`);
 
 for (num of numberButtons) {
   num.addEventListener("click", (e) => {
-    let addDigit = e.target.value;
-    numberString += addDigit;
-    let displayWindow = parseInt(numberString);
-    console.log(typeof displayWindow);
-    display.value = parseInt(displayWindow);
-    console.log(typeof display.value);
+    getNumber(e);
   });
 }
 
 clearButton.addEventListener("click", clear);
 changeSignButton.addEventListener("click", changeSign);
+
 addButton.addEventListener("click", () => {
-  display.value = "";
-  display.value = operate("+", numberString, numberString);
+  equationArray.push(parseInt(display.textContent));
+  equationArray.push("+");
+  numberString = "";
+  console.log(equationArray);
+  display.textContent = "";
+});
+
+equalsButton.addEventListener("click", () => {
+  equationArray.push(parseInt(display.textContent));
+  console.log(equationArray);
+  numberString = equationArray.shift();
+  console.log(numberString);
+  operator = equationArray.shift();
+  console.log(operator);
+  numberString2 = equationArray.shift();
+  console.log(numberString2);
+  display.textContent = operate(operator, numberString, numberString2);
 });
